c2f44f23d653f957e94c61b3568f3a197ef1ceac
khalome <khalome@vm1>
2023-09-29 17:17:11 +0000

    clean up

--- Program.cs
+++ Program.cs
@@ -35,11 +35,8 @@ namespace CtekDev_ManagedIdentity
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
             app.UseHttpsRedirection();
 
